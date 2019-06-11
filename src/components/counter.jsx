29604f8e4f80ccfdf0cefc6d3cd9a66f43c10fea
Toby Chow <tobychow98@gmail.com>
2019-06-11 16:18:07 -0500

    Passing Event Arguments
    Code is not compiling correctly

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,15 +6,17 @@ export default class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
-    handleIncrement = () => {
+    handleIncrement = product => {
+        console.log(product);
         this.setState({count: this.state.count + 1});
-    }
-
+    };
+    
     render() {
         return (
             <React.Fragment>
                 <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement(product)} 
+                className="btn btn-secondary btn-sm">Increment</button>
                 <ul>
                     { this.state.tags.map(tag => <li key={tag}>{ tag }</li>)  }
                 </ul>
